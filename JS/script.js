cbe560a6b9093d9bd7dd384d5f49a7794983379d
FaelPSilva <164876697+FaelPSilva@users.noreply.github.com>
2024-04-26 12:04:24 -0300

    atualização geral

--- JS/script.js
+++ JS/script.js
@@ -1,68 +1,81 @@
 var showButton = document.getElementById("calculator") 
-var resultContainer = document.querySelector(".result-container")
-var inputBTN = document.querySelector(".input")
 var formBTN = document.querySelector("imcForm")
-function calculate (){
+function calculate(){
 
     event.preventDefault();
-           
+         
     var weight = document.getElementById("weight").value; //varivel peso
     var height = document.getElementById("height").value; // variavel altura
-    console.log(weight);
-    console.log(height);
-
-    if (weight.trim() === '' || height.trim() === '') {
-        alert("Por favor, preencha todos os campos de entrada.");
-        return;}
-
-    var imc = weight / (height **2); //calculo
-
-    var resultIMC = document.getElementById("resultIMC") //referencia para a tag html
-    var imcSituationText = document.getElementById("imcSituationText")
-   
-   
     
-
-    if (imc<18.5){
-        imcSituationText.textContent = " Você está abaixo do peso"
-        imcSituationText.className = "low"
-    } else if(imc<24.9){
-        imcSituationText.textContent = " Peso Ideal"
-        imcSituationText.className = "good"
-    } else if(imc<29.9){
-       imcSituationText.textContent = " Sobre Peso"
-       imcSituationText.className = "regular"
-    } else if(imc<34.9){
-        imcSituationText.textContent = " Obesidade"
-        imcSituationText.className = "madium"
-    } else if(imc<39.9){
-        imcSituationText.textContent = " Obesidade Morbida"
-        imcSituationText.className = "bad"
-    } else {
-       imcSituationText.textContent = " Obesidade Grau III"
-       imcSituationText.className = "veryBad"
+    
+    if (weight.trim() == '' || height.trim() == '') {
+        alert("Por favor, preencha todos os campos de entrada."); //varifica se os campos estão 
+        return false;
     }
 
-    resultIMC.textContent = imc.toFixed(2); //limita a quantidade de caracteres
+     
+
+        var imc = weight / (height **2); //calculo
+    
+        var resultIMC = document.getElementById("resultIMC") //referencia para a tag html
+        
+        
+
+        var imcSituationText = document.getElementById("imcSituationText") //referencia para a tag html
+       
+       
+        document.querySelector(".result-container")
+        .classList.toggle('show');
+        //style.display = "flex" ;
+
+        document.querySelector(".input").style.display = "none"
+
+        
+        
+    
+        if (imc<18.5){
+            imcSituationText.textContent = " Você está abaixo do peso"
+            imcSituationText.className = "low"
+        } else if(imc<24.9){
+            imcSituationText.textContent = " Peso Ideal"
+            imcSituationText.className = "good"
+        } else if(imc<29.9){
+           imcSituationText.textContent = " Sobre Peso"
+           imcSituationText.className = "regular"
+        } else if(imc<34.9){
+            imcSituationText.textContent = " Obesidade"
+            imcSituationText.className = "madium"
+        } else if(imc<39.9){
+            imcSituationText.textContent = " Obesidade Morbida"
+            imcSituationText.className = "bad"
+        } else {
+           imcSituationText.textContent = " Obesidade Grau III"
+           imcSituationText.className = "veryBad"
+        }
+    
+        resultIMC.textContent = imc.toFixed(2); //limita a quantidade de caracteres
+    
 } 
+
+    document.getElementById("limpar").addEventListener("click", function()
+    {
+    document.getElementById('weight').value = '';
+    document.getElementById('height').value = '';   
+    }
+    );
    
-    showButton.addEventListener("click", function(event) {
-       //exibe a div com class container-result
+    // showButton.addEventListener("keyup", function(event) {
+       
+    // }); 
 
-        resultContainer.style.display = "flex";
-        inputBTN.style.display = "none"
-
-        event.preventDefault()
-    }); 
-
-    formBTN.addEventListener("click", function(event) {
-        //exibe a div com class container-result
+    // formBTN.addEventListener("click", function(event) {
+    //     //exibe a div com class container-result
  
-       event.preventDefault()
+    //    event.preventDefault()
 
        
  
-     });
+    //  });
 
      
 
